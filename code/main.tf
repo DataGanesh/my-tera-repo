a2779320d6d86b6e6259c0df3a1f8407f9a75a48
Ganesh <gnivdunge@gmail.com>
2024-02-19 14:04:43 +0000

    code successful

---
+++ code/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region  = var.region
+  profile = "ganesh"
+}
+
+module "vpc" {
+  source                       = "../modules/vpc"
+  region                       = var.region
+  project_name                 = var.project_name
+  vpc_cider                    = var.vpc_cider
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
