6ce43c699b3e976c4f63e21d12aecfaf244a6501
Ganesh <gnivdunge@gmail.com>
2024-02-20 16:03:03 +0000

    internet gateway cerated and pushed on gitlab

--- code/main.tf
+++ code/main.tf
@@ -15,3 +15,20 @@ module "vpc" {
   private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
+
+
+# create nat gateway
+
+module "nat_gateway" {
+  source                        = "../modules/nat_gateway"
+  public_subnet_az1_id          = module.vpc.public_subnet_az1_id
+  internet_gateway              = module.vpc.internet_gateway
+  public_subnet_az2_id          = module.vpc.public_subnet_az2_id
+  vpc_id                        = module.vpc.vpc_id
+  private_app_subnet_az1_id     = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id    = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id     = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id    = module.vpc.private_data_subnet_az2_id
+
+  
+}
